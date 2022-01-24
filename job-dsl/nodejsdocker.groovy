d9ee0791dddc6d82c3c99216066a491388f936ba
Richard Matheus <richard_matheus2004@yahoo.com.br>
2022-01-24 17:07:22 -0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('richardgrs/docker-nodejs-test')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
